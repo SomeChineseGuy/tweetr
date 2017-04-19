e57bdadd2f5e0825b318ce91a28ce49d094e946a
vagrant <alvin.cl.ng@gmail.com>
2017-04-19 01:25:54 +0000

    got other tweets done with

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(function () {
+
+  const maxCount = 140;
+  $('textarea').on('input', function() {
+    let length = $(this).val().length;
+    length = maxCount - length;
+    $('.counter').text(length);
+    $('.counter').css('color', 'black');
+    if (length < 0) {
+      $('.counter').css('color', 'red');
+    }
+  });
+  $(".new-tweet form").submit(function() {
+    $(".counter").text(maxLength);
+  });
+});
